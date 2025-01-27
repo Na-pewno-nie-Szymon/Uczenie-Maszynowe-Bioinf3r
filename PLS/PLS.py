2e9a730e4144e140b34b930de98bb2018ce90aa7
Na-pewno-nie-Szymon <ciastkojablkowe@gmail.com>
2025-01-27 15:40:56 +0100

    PCR speedrun

--- PLS/PLS.py
+++ PLS/PLS.py
@@ -1,56 +1,30 @@
 '''
-1.  Zaimportowanie autoskalowanego zestawu danych.
-    Arkusze Yt oraz Yv zawierają wartości modelowanej wielkości
-    (energia absorbcji aminokwasu na powierzchni nanocząstki złota)
-    odpowiednio dla związków ze zbioru uczącego i awlidacyjnego;
-    arkusze Xt i Xv zawierają deskryptory obliczone odpowiednio dla
-    związków zbioru uczącego i walidacyjnego
-
-2.  Przeprowadzenie modelowania PLS (jedna ukryta zmienna; 3 deskryptory:
-    energia HOMO, polaryzowalność i topologiczny obszar powierzchni)
-
-3.  Wykreślenie ypred od yobs z podziałem na zbiór uczący i walidacyjny (legenda)
-
-4.  Obliczenie statystyk: R^2, RMSEc Q^2 CVloo RMSE CVloo, Q^2 Ex RMSE Ex
-
-5.  Zbudować modele MLR i PRC, powtórzyć dla nich kroki 3 i 4, a następnie
-    porównać wyniki uzyskane wszystkimi trzema metodami.
-'''
-
-
-'''
 TODO:
-- check what's done 
-- code review done parts 
-- check and write what need to be done, updated and/or removed acording to .pdf file
-- write readme.md file with instructions how to install, setup and use program
-- push to git
-- send email
-
+    [x] Zaimportuj zestaw danych
+    [x] autoskalowanie
+    [x] przeprowadź modelowanie PLS (jedna zmienna ukryta, 3 deskryptory,
+        energia HOMO, polaryzowalność i topologiczny obszar powierzchni)
+    [ ] Wykreślenie ypred od yobs z podziałem na zbiór uczący i walidacyjny (legenda)
+    [ ] Obliczenie statystyk: R2, RMSEc, Q2cvloo, RMSEcvloo, Q2ex, RMSEex
+    [ ] Zbudować modele MLR i PCR, powtórzyć dla nich kroki 3 i 4, a następnie
+        porównać wyniki uzyskane wszystkimi trzema metodami
+    [ ] ypred od yobs dla MLR
+    [ ] ypred od yobs dla PCR
+    [ ] statystyki dla MLR
+    [ ] statyskyki dla PCR
+    [ ] porównanie wyników PCR, MLR, PLS
 '''
 import pandas as pd
-import os
 from sys import argv
 from sklearn.preprocessing import StandardScaler
 from sklearn.cross_decomposition import PLSRegression
 from sklearn.metrics import mean_squared_error
 
 def data_loader(path: str):
-    try:
-        if not os.path.exists(path):
-            raise FileNotFoundError(f'Plik {path} does not exist!')
-        
-        Yt = pd.read_excel(path, sheet_name="Yt")
-        Yv = pd.read_excel(path, sheet_name="Yv")
-        Xt = pd.read_excel(path, sheet_name="Xt")
-        Xv = pd.read_excel(path, sheet_name="Xv")
-
-    except FileNotFoundError as error:
-        print(f'Error: {error}')
-        exit()
-    except Exception as error:
-        print(f"Unknown error ocured: {error}")
-        exit()
+    Yt = pd.read_excel(path, sheet_name="Yt")
+    Yv = pd.read_excel(path, sheet_name="Yv")
+    Xt = pd.read_excel(path, sheet_name="Xt")
+    Xv = pd.read_excel(path, sheet_name="Xv")
 
     return [Yt, Yv, Xt, Xv]
 
