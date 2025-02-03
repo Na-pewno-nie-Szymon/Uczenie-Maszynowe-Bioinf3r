ea9a466adcb3affb574a3f2aa8f2159110dfe1e5
Na-pewno-nie-Szymon <ciastkojablkowe@gmail.com>
2025-02-03 15:07:53 +0100

    aaa

--- SVM/SMV.py
+++ SVM/SMV.py
@@ -25,12 +25,14 @@
         marginesem
     [ ] 7 Krótko skomentuj uzyskane wyniki
 '''
-
-
 import numpy as np
 import pandas as pd
 import seaborn as sns
 import matplotlib.pyplot as plt
+from sklearn.svm import SVC
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 
 def data_loader(PATH: str) -> pd.DataFrame:
     data = pd.read_csv(PATH)
@@ -44,13 +46,66 @@ def pairplots(data: pd.DataFrame):
     sns.pairplot(data=data, hue='species')
     plt.show()
 
+def plot_svm_decision_boundary(model, X, y, title):
+    plt.figure(figsize=(8, 6))
+    plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.coolwarm, edgecolors='k')
+    
+    ax = plt.gca()
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+    
+    xx, yy = np.meshgrid(np.linspace(xlim[0], xlim[1], 50),
+                         np.linspace(ylim[0], ylim[1], 50))
+    Z = model.decision_function(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+    
+    ax.contour(xx, yy, Z, colors='k', levels=[-1, 0, 1], alpha=0.5,
+               linestyles=['--', '-', '--'])
+    
+    sv = model.support_vectors_
+    plt.scatter(sv[:, 0], sv[:, 1], s=100, facecolors='none', edgecolors='k', label='Support Vectors')
+    plt.legend()
+    plt.title(title)
+    plt.show()
+
 if __name__=='__main__':
     FILE_PATH = './data/penguins_size.csv'
 
-    # data := [species,island,bill_length_mm,bill_depth_mm,flipper_length_mm,body_mass_g,sex]
     data = data_preprocessing(data_loader(FILE_PATH))
     pairplots(data=data)
 
-
-    print(type(data), data.shape)
-    print(data)
+    # Wybór dwóch cech i dwóch gatunków do analizy liniowej
+    selected_species = data[data['species'].isin(['Adelie', 'Chinstrap'])]
+    X = selected_species[['bill_length_mm', 'bill_depth_mm']].values
+    y = selected_species['species'].apply(lambda x: 1 if x == 'Adelie' else 0).values
+    
+    X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+    
+    scaler = StandardScaler()
+    X_train = scaler.fit_transform(X_train)
+    X_test = scaler.transform(X_test)
+    
+    # Model liniowy
+    svm_linear = SVC(kernel='linear')
+    svm_linear.fit(X_train, y_train)
+    y_pred = svm_linear.predict(X_test)
+    accuracy_linear = accuracy_score(y_test, y_pred)
+    print(f'Linear Kernel Accuracy: {accuracy_linear:.2f}')
+    plot_svm_decision_boundary(svm_linear, X_train, y_train, "Linear Kernel")
+    
+    # Modele dla danych nieliniowo separowalnych
+    svm_poly = SVC(kernel='poly')
+    svm_poly.fit(X_train, y_train)
+    y_pred_poly = svm_poly.predict(X_test)
+    accuracy_poly = accuracy_score(y_test, y_pred_poly)
+    
+    svm_rbf = SVC(kernel='rbf')
+    svm_rbf.fit(X_train, y_train)
+    y_pred_rbf = svm_rbf.predict(X_test)
+    accuracy_rbf = accuracy_score(y_test, y_pred_rbf)
+    
+    print(f'Polynomial Kernel Accuracy: {accuracy_poly:.2f}')
+    print(f'RBF Kernel Accuracy: {accuracy_rbf:.2f}')
+    
+    # Wizualizacja dla jądra liniowego (nieliniowo separowalne dane)
+    plot_svm_decision_boundary(svm_linear, X_train, y_train, "Non-Linearly Separable Data - Linear Kernel")
